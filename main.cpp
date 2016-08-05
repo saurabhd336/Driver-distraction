98cb980607f504e907398a8d17ad3a587743aa99
Saurabh Dubey <saurabhd336@gmail.com>
2016-08-05 13:06:39 +0530

    Added video

--- main.cpp
+++ main.cpp
@@ -32,99 +32,48 @@ struct svm_node *x_space;
 struct svm_node testnode[51];
 
 
-void svm_predict(std::vector<double> &angles)
+string svm_predict(std::vector<double> &angles)
 {
-
-	//for(int  i = 0; i < angles.size(); i++)
-	//	cout<<angles[i]<<"\n";
-	//ifstream remp("Testing_data_matReduced.csv");
-	
-	//count = 0;
-	// while(remp)
-	// {
-	// 	remp>>str;
-	// 	count++;
-	// }
-	// count--;
-	//prob.l = 1;
-	//remp.close();
-	//cout<<count<<"\n";
-//	test.y = Malloc(double,prob.l);
-	
-	//fin.open("Testing_data_matReduced.csv");
-	
-	//double correct = 0;
-	//cout<<"Here\n";
 	for(int i = 0; i < 50; i++)
 	{
 		testnode[i].index = i;
 		testnode[i].value = angles[i];
 	}
 	testnode[50].index = -1;
-	// 	int len = 0;
-	// 	fin >> str;
-	// 	vals = strtok(str, ",");
-	// 	while(vals)
-	// 	{
-	// 		value = convert(vals);
-	// 		//cout<<value<<" ";
-	// 		vals = strtok(NULL , ",");
-	// 		if(len == 50)
-	// 			break;
-	// 		 testnode[len].index = len;
- //   			 testnode[len++].value = value;
-	// 	}
-	// //	test.y[i] = value;
-	// 	testnode[50].index = -1; 
-		
-		//for(int i = 0; i < 50; i++)
-		//	cout<<testnode[i].index<<" "<<testnode[i].value<<"\n";
-		//cout<<"Example # "<<i<<"\n";
-	//cout<<"Here\n";
 	double retval = svm_predict(model,testnode);
-	//cout<<retval<<"\n";
 	int val = retval;
 
 	switch(val)
 	{
 		case 0:
-				cout<<"Centre Stack\n";
+				return "Centre Stack";
 				break;
 		case 1:
-				cout<<"Instrument stack\n";
+				return "Instrument stack";
 				break;
 		case 2:
-				cout<<"Left\n";
+				return "Left";
 				break;
 		case 3:
-				cout<<"Right\n";
+				return "Right";
 				break;
 		case 4:
-				cout<<"Road\n";
+				return "Road";
 				break;
 		case 5:
-				cout<<"Rear view mirror\n";
+				return "Rear view mirror";
 				break;
 		case 6:
-				cout<<"Up\n";
+				return "Up";
 				break;
 		case 7:
-				cout<<"Down\n";
+				return "Down";
 				break;
 		default:
-				cout<<"Not defined\n";
+				return "Not defined";
 				break;
 	}
-		//printf("retval: %f actualval: %f\n",retval,value);
-		//if(retval == value)
-		//	correct++
-  	
-  	//cout<<(correct) / (double)(count)<<"\n";
-
-
-	//This works correctly:
-	//double retval = svm_predict(model,testnode);
-//	printf("retval: %f\n",retval);
+	return "";
 }
 
 //Calculate angle of triangle given three coordinates c++ code
@@ -141,14 +90,8 @@ void TriangleAngleCalculation(std::vector<double> &angles, int x1, int y1, int x
 
 	angle1 = (180 * acos((dist2 + dist3 - dist1) / (2 * sqrt(dist2 * dist3)))) / PI;
 	angle2 = (180 * acos((dist1 + dist3 - dist2) / (2 * sqrt(dist1 * dist3)))) / PI;
-	//angle3 = (180 * acos((dist1 + dist2 - dist3) / (2 * sqrt(dist1 * dist2)))) / PI;
-	//cout << "Angles are:\n";
 	angles.push_back(angle1);
 	angles.push_back(angle2);
-	//cout << angle1 << " " << angle2 << " " << angle3 << "\n";
-
-	//fout_angles << angle1 << "," << angle2 << ",";// << angle3 << " ";
-
 }
 
 
@@ -289,66 +232,43 @@ int triangulation(Mat img, full_object_detection shape, int arr[20])
 	line(img, vec[13], vec[9], CV_RGB(0, 0, 255), 1, CV_AA, 0);
 	line(img, vec[9], vec[0], CV_RGB(0, 0, 255), 1, CV_AA, 0);
 	TriangleAngleCalculation(angles, vec[13].x, vec[13].y, vec[0].x, vec[0].y, vec[9].x, vec[9].y);
-
-	//cout<<angles.size()<<"\n";
-	//string win_delaunay = "Delaunay Triangulation";
 	
-	svm_predict(angles);
+	string result = svm_predict(angles);
 
+	putText(img, result, Point(  img.cols / 2, img.rows / 4), 1, 2, Scalar(0, 0, 255), 3, 8);
 
 	cv_image<bgr_pixel> x(img);
 	array2d<rgb_pixel> temp;
 	assign_image(temp, x);
 	win_triangles.clear_overlay();
 	win_triangles.set_image(temp);
-	//imshow("win_delaunay", img);
-	//waitKey(0);
 	return 0;
 }
 
 int main()
 {
-	//Mat x;
-	//testnode = new svm_node[51];
 	int check;
 	try
 	{
 		model = svm_load_model("model.bin");
-		//fout_angles.open("angles.txt", ios::trunc);
-		// This example takes in a shape model file and then a list of images to
-		// process.  We will take these filenames in as command line arguments.
-		// Dlib comes with example images in the examples/faces folder so give
-		// those as arguments to this program.
-
-		// We need a face detector.  We will use this to get bounding boxes for
-		// each face in an image.
 		cv::VideoCapture vid(0);
 		frontal_face_detector detector = get_frontal_face_detector();
-		// And we also need a shape_predictor.  This is the tool that will predict face
-		// landmark positions given an image and face bounding box.  Here we are just
-		// loading the model from the shape_predictor_68_face_landmarks.dat file you gave
-		// as a command line argument.
 		shape_predictor sp;
 		deserialize("shape_predictor_68_face_landmarks.dat") >> sp;
 		int arr[] = { 0,2,4,6,8,10,12,14,16,19,24,27,30,31,33,35,48,51,54};
 		image_window win, win_faces;
-		// Loop over all the images provided on the command line.
+
 		while(!win.is_closed())
 		{
-			cv::Mat temp;
-        	vid >> temp;
+			cv::Mat temp2;
+        	vid >> temp2;
+        	cv::Mat temp;
+        	flip(temp2, temp, 1);
         	cv_image<bgr_pixel> image(temp);
 			array2d<rgb_pixel> img;
 			assign_image(img, image);
 			
-			//load_image(img, argv[i]);
-			
-			// Make the image larger so we can detect small faces.
-			//pyramid_up(i
-			// Now tell the face detector to give us a list of bounding boxes
-			// around all the faces in the image.
 			std::vector<dlib::rectangle> dets = detector(img);
-			//std::cout << "Number of faces detected: " << dets.size() << endl;
 			check = 0;
 			// Now we will go ask the shape_predictor to tell us the pose of
 			// each face we detected.
@@ -358,42 +278,10 @@ int main()
 			{
 				check = 1;
 				full_object_detection shape = sp(img, dets[0]);
-				//std::cout << "number of parts: " << shape.num_parts() << endl;
-				//std::cout << "pixel position of first part:  " << shape.part(0) << endl;
-				//std::cout << "pixel position of second part: " << shape.part(1) << endl;
-				// You get the idea, you can get all the face part locations if
-				// you want them.  Here we just store them in shapes so we can
-				// put them on the screen.
 				shapes.push_back(shape);
 				triangulation(temp, shape, arr);
 			}
-			
-
-			// Now let's view our face poses on the screen.
-			//win.clear_overlay();
-			//win.set_image(img);
-			//win.add_overlay(render_face_detections(shapes));
-			
-
-			// We can also extract copies of each face that are cropped, rotated upright,
-			// and scaled to a standard size as shown here:
-			//dlib::array<array2d<rgb_pixel> > face_chips;
-			//extract_image_chips(img, get_face_chip_details(shapes), face_chips);
-			//win_faces.set_image(tile_images(face_chips));
-			//string temp_filename = strcat(argv[i], "_o.jpg");
-
-			//save_bmp(img, temp_filename);
-                
-			//std::cout << "Hit enter to process the next image..." << endl;
-			//cin.get();
-			//if (check == 1)
-			//fout_angles << "\n";
-			//cnt++;
 		}
-		//fout_angles << "Here\n";
-		//fout_angles.clear();
-		
-		//fout_angles.close();
 		getchar();
 	}
 
